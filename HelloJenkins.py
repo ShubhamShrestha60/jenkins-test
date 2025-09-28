4205ad4e4d315db9dce7ac048e737924cf652c2b
ShubhamShrestha60 <166488190+ShubhamShrestha60@users.noreply.github.com>
2025-09-28 22:49:32 +0545

    errore HelloJenkins.py

--- HelloJenkins.py
+++ HelloJenkins.py
@@ -1,4 +1,4 @@
-def main():
+def mai():
     print("Hello from Shubham Shrestha")
     print("This is my test script.")
     print("Checking if the pool SCM works after i make some changes to the code")
