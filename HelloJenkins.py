91f6426cb82ae8a90d9a810240e0ad189003468a
ShubhamShrestha60 <166488190+ShubhamShrestha60@users.noreply.github.com>
2025-09-28 17:02:14 +0545

    Update HelloJenkins.py

--- HelloJenkins.py
+++ HelloJenkins.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello from Shubham Shrestha")
     print("This is my test script.")
+    print("Checking if the pool SCM works after i make some changes to the code")
 
 if __name__ == "__main__":
     main()
